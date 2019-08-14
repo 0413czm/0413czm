621756e589011e49d99699464240b80ce3fd684b
2514939946@qq.com <2514939946@qq.com>
2019-08-14 14:06:48 +0800

    数据模拟初始化

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 window.onload = function() {
+  // 渲染主页con_left数据
+  $('.content-box .con_left ul').html(template('indexTmp',indexData));
   var mySwiper = new Swiper ('.swiper-container', {
-    direction: 'horizontal', // 垂直切换选项
+    direction: 'horizontal', // 左右切换选项
     loop: true, // 循环模式选项
-    
     // 如果需要分页器
     pagination: {
       el: '.swiper-pagination',
+      clickable :true,
     },
-    
-    // 如果需要滚动条
-    
-  })        
+    autoplay: true
+  })              
 }
+
+
