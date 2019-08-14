e8f54d4c7b0d7d051ed31fcbe8764476763c56e4
2514939946@qq.com <2514939946@qq.com>
2019-08-14 18:20:57 +0800

    留言框完成

--- js/message.js
+++ js/message.js
@@ -2,3 +2,11 @@
 console.log(message);
 
 $('.content-box .con_left ul').html(template('msgTmp',message));
+
+// 关闭模态框
+$('.modal-header .shanchu').on('click',function (){
+    $('.modal').css('display','none');
+})
+$('.replay').on('click',function (){
+    $('.modal').css('display','block');
+})
